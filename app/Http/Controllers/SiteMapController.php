d255b8e186fd6173aa767190e24199b0183a9ca0
georgebush <wodofyuina@qq.com>
2018-11-08 14:14:54 +0800

    s

--- app/Http/Controllers/SiteMapController.php
+++ app/Http/Controllers/SiteMapController.php
@@ -7,14 +7,14 @@ class SiteMapController extends Controller
 {
     public function index()
     {
-        for ($i=0;$i<1500;$i++){
-            $pre =['http://www.nefi.com.cn/clu','http://www.nefi.com.cn/clu'];
-            $data[] = array_random($pre).str_random(4).rand(100000,999999).'.html';
+        for ($i=0;$i<200;$i++){
+            $pre = env('MULU_HOST').env('MULU');
+            $mulu[] = $pre.str_random(6).'/';
         }
 
-        for ($i=0;$i<200;$i++){
-            $pre =['http://www.nefi.com.cn/clu','http://www.nefi.com.cn/clu'];
-            $mulu[] = array_random($pre).str_random(4).'/';
+        for ($i=0;$i<1500;$i++){
+            $pre = env('MULU_HOST').env('MULU');
+            $data[] = $pre.str_random(6).'/'.rand(100000,999999).'/';
         }
 
         return response()->view('sitemap', [
@@ -22,13 +22,4 @@ class SiteMapController extends Controller
             'mulu' => $mulu
         ])->header('Content-Type', 'text/xml');
     }
-
-    public function test()
-    {
-        for ($i=0;$i<500;$i++){
-            echo 'http://zikao.eol.cn/clu'.str_random(3).rand(1000000,9999999).'/';
-            echo '<br>';
-        }
-    }
-
 }
